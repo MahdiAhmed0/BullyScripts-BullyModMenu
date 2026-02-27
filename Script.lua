63fdf9b37c9943273984110f439ff155fd96aa6b
MahdiAhmed0 <143342068+MahdiAhmed0@users.noreply.github.com>
2026-02-27 15:39:34 +0300

    Update Script.lua
    that's a rewrite and enhancement of the old mod menu

--- Script.lua
+++ Script.lua
@@ -1,56 +1,290 @@
+switch = 0
+JimmySpeed = 100
+onMission = false
+endOfTest = false
+FlagHasBeenSet = false
+tempFlag = 0
 
---- All of those codes were inside a main function called Main(), and another main function called MissionSetup(). These are the only functions that the game can recognize and run....
--- and for sure you can run another undircted function from a Main() or MissionSetup().
--- You can learn a lot by understanding the code. It's a small chunk of the original script, but it contains a few concepts that can assist you in any future project, really good concepts.
+function MissionSetup()
+    PlayerSetControl(0)
+    PlayerSetPosXYZArea(270, -110, 6, 0)
+    repeat
+        Wait(0)
+    until not AreaIsLoading()
+    CameraSetWidescreen(true)
+end
 
---- missing parts
-SimpleMenu = {
-    {text = "Reset Camera", Func = testingBro1},
-    {text = "Health", Func = testingBro2},
-    {text = "Get Spud Gun", Func = testingBro3},
-    {text = "Punishment Toggle", Func = testingBro4},
-    {text = "Kill Ped", Func = testingBro5},
-}
-Selection = 1
-repeat
-    if IsButtonBeingPressed(0, 0) then
-        Selection = Selection - 1
-        if Selection < 1 then
-            Selection = table.getn(SimpleMenu)
+
+function main()
+    TextPrintString("Yo ******! Welcome to bully again", 2, 1)
+    Wait(2000)
+    PlayerSetControl(1)
+    CameraSetWidescreen(false)
+    TextPrintString("You can move now!", 3, 1)
+    CreateThread("menu")
+end
+
+
+function testStim()
+    testPed = PedCreateXYZ(31, 270, -110, 6)
+    Wait(1000)
+    PedWander(testPed, true)
+    CreateThread("FlagsMenu")
+    repeat
+        Wait(0)
+    until endOfTest
+    TextPrintString("The test is over now!", 3, 1)
+    Wait(2000)
+    MissionSucceed()
+
+end
+
+function FlagsMenu()
+    Selection = 2
+    repeat
+        TextPrintString("Current Flag ID:" .. Selection, 1, 2)
+        if IsButtonBeingPressed(0, 0) then
+            Selection = Selection - 1
+            if Selection < 1 then
+                Selection = 132
+            end
+        elseif IsButtonBeingPressed(1, 0) then
+            Selection = Selection + 1
+            if Selection > 132 then
+                Selection = 1
+            end
+        elseif IsButtonBeingPressed(3, 0) then
+            if Selection == 1 then
+                endOfTest = true
+                return
+            end
+            if FlagHasBeenSet == false then
+                tempFlag = Selection
+                PedSetFlag(gPlayer, tempFlag, true)
+                FlagHasBeenSet = true
+                TextPrintString("the flag has been activated!", 3, 1)
+            else
+                PedSetFlag(gPlayer, tempFlag, false)
+                PedSetFlag(gPlayer, Selection, true)
+                tempFlag = Selection
+                TextPrintString("the flag has been activated! (Deactivated the prev one!)", 3, 1)
+            end
         end
-        TextPrintString(SimpleMenu[Selection].text,4,1)
-    elseif IsButtonBeingPressed(1, 0) then
-        Selection = Selection + 1
-        if Selection > table.getn(SimpleMenu) then
-            Selection = 1
+        Wait(0)
+    until not Alive
+
+end
+
+
+
+
+function StartMission()
+    Stage = 1
+    Wait(1000)
+    TextPrintString("Follow the marks on MiniMap!", 3, 1)
+    local blip1 = BlipAddXYZ(302, -72, 5, 0)
+    enemies = {}
+    for i = 1, 3 do
+        enemies[i] = PedCreateXYZ(31, 302 + (i/2), -72, 5)
+    end
+    CreateThread("Informations")
+    local x, y = 302, -72
+    local r1 = x + 2
+    local r2 = y + 2
+    local r3 = x - 2
+    local r4 = y - 2
+    repeat
+        Wait(0)
+    until PedInRectangle(gPlayer, r1, r2, r3, r4)
+    Stage = 2
+    BlipRemove(blip1)
+    TextPrintString("Beat those!", 3, 1)
+    tempBlips = {}
+    for i = 1, 3 do
+        PedSetPedToTypeAttitude(enemies[i], 13, 0)
+        PedAttackPlayer(enemies[i], 3)
+        tempBlips[i] = AddBlipForChar(enemies[i], 0, 26, 4)
+    end
+    repeat
+        Wait(0)
+    until PedsIsDead()
+
+    TextPrintString("There is another mark.", 3, 1)
+    local blip2 = BlipAddXYZ(323, -98, 6, 0)
+    local x, y = 323, -98
+    local r1 = x + 2
+    local r2 = y + 2
+    local r3 = x - 2
+    local r4 = y - 2
+    repeat
+        Wait(0)
+    until PedInRectangle(gPlayer, r1, r2, r3, r4)
+    Stage = 3
+    BlipRemove(blip2)
+    TextPrintString("Last mark!!!", 3, 1)
+    local blip3 = BlipAddXYZ(472, -89, 5, 0)
+    local x, y = 472, -89
+    local r1 = x + 2
+    local r2 = y + 2
+    local r3 = x - 2
+    local r4 = y - 2
+    repeat
+        Wait(0)
+    until PedInRectangle(gPlayer, r1, r2, r3, r4)
+    MissionSucceed()
+end
+
+
+
+function PedsIsDead()
+    for i = 1, 3 do
+        if not PedIsDead(enemies[i]) and PedIsValid(enemies[i]) then
+            return false
         end
-        TextPrintString(SimpleMenu[Selection].text,4,1)
-    elseif IsButtonBeingPressed(3, 0) then
-        SimpleMenu[Selection].Func()
+    end
+    return true
+
+end
+
+
+
+
+
+
+
+
+
+
+function Informations()
+    while true do
+        Wait(0)
+        if Stage == 1 then
+            if IsButtonBeingPressed(1, 0) then
+                TextPrintString("Please don't be 87loon, it's a STAGE 1!", 3, 1)
+            end
+        elseif Stage == 2 then
+            if IsButtonBeingPressed(1, 0) then
+                TextPrintString("you are making me tired, it's a STAGE 2!", 3, 1)
+            end
+        elseif Stage == 3 then
+            if IsButtonBeingPressed(1, 0) then
+                TextPrintString("SHIIIIIAAAT, it's a STAGE 3!", 3, 1)
+            end
+        end
+    end
+end
+
+
+
+
+
+
+function menu()
+    SimpleMenu = {
+        {text = "Say Hi !", Func = testingBro1},
+        {text = "Health", Func = testingBro2},
+        {text = "Get Spud Gun", Func = testingBro3},
+        {text = "Punishment Toggle", Func = testingBro4},
+        {text = "Kill Ped", Func = testingBro5},
+        {text = "Jimmy's Speed", Func = testingBro6},
+        {text = "Start test mission", Func = testingBro7},
+        {text = "Start Test Stimulus FUNCTION !", Func = testingBro8},
+        {text = "Start Test Vehiacle moving from ped!", Func = testingBro9},
+    }
+    Selection = 1
+    repeat
+        if IsButtonBeingPressed(0, 0) then
+            Selection = Selection - 1
+            if Selection < 1 then
+                Selection = table.getn(SimpleMenu)
+            end
+            TextPrintString(SimpleMenu[Selection].text,4,1)
+        elseif IsButtonBeingPressed(1, 0) then
+            Selection = Selection + 1
+            if Selection > table.getn(SimpleMenu) then
+                Selection = 1
+            end
+            TextPrintString(SimpleMenu[Selection].text,4,1)
+        elseif IsButtonBeingPressed(3, 0) then
+            SimpleMenu[Selection].Func()
+        end
+    
+        Wait(0)
+    until not Alive
+
+end
+
+
+function testingBro1()
+    TextPrintString("HI !", 3, 2)
+end
+
+function testingBro2()
+    local JimmyHealth = PedGetMaxHealth(gPlayer)
+    PlayerSetHealth(JimmyHealth)
+end
+
+
+function testingBro3()
+    PedOverrideStat(gPlayer, 45, 305)
+end
+
+function testingBro4()
+    if switch == 0 then
+        DisablePunishmentSystem(true)
+        PlayerSetPunishmentPoints(0)
+        TextPrintString("Punishment system is disabled", 3, 2)
+        switch = 1
+    elseif switch == 1 then
+        DisablePunishmentSystem(false)
+        TextPrintString("Punishment system is enabled", 3, 2)
+        switch = 0
+    else
+        TextPrintString("the function went wrong (Punishment Toggle, Func = testingBro4)", 3, 2)
+    end
+end
+
+function testingBro5()
+    local JimmyTarget = PedGetTargetPed(gPlayer)
+    if not PedIsDead(JimmyTarget) then
+        PedApplyDamage(JimmyTarget, 100000)
+    end
+end
+
+
+function testingBro6()
+    JimmySpeed = JimmySpeed + 25
+    GameSetPedStat(gPlayer, 20, JimmySpeed)
+    if JimmySpeed > 400 then
+        JimmySpeed = 100
+        GameSetPedStat(gPlayer, 20, JimmySpeed)
+    end
+    TextPrintString("Speed:" .. JimmySpeed .. "%", 3, 2)
+end
+
+
+function testingBro7()
+    if onMission == false then
+        onMission = true
+        StartMission()
+    end
+end
+
+
+
+
+function testingBro8()
+    if onMission == false then
+        onMission = true
+        testStim()
     end
 
-    Wait(0)
-until not Alive
---- missing parts
---- 
---- 
---- 
-function testingBro4()
-if PunishmentV < 1 then
-    PunishmentV = 1
-    DisablePunishmentSystem(true)
-    PedSetPunishmentPoints(gPlayer, 0)
-elseif PunishmentV > 0 then
-    PunishmentV = 0
-    DisablePunishmentSystem(false)
 end
+
+
+function testingBro9()
+    repeat
+        SoundPlay3D(270, -110, 6, 'NavDwn')
+        Wait(500)
+    until not Alive
 end
-PunishmentV = 0
-
---- missing parts
-
--- The story behind this script: I was developing a script 'Mission' and there was too much to debug. So, I built this menu to assist me with debugging my 'Mission'.
--- The most helpful debugging feature was the 'Reset Camera' option because the camera was glitching all the time. As a result, I had to debug the camera most of the time.
-
--- IMPORTANT: There is no license for this script. Much of its original code is missing, and it was testing code not intended to be published by any means.
--- Developed By : Fibonacci
